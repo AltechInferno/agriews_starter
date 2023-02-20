871d332ab55ded48aeacb619ef242c82942fa53e
Alhassan Tijani <jhaeti@gmail.com>
2023-02-20 18:30:23 +0000

    Added and imported more components

--- src/views/page-1/components/index.js
+++ src/views/page-1/components/index.js
@@ -3,5 +3,27 @@ import AddQuestion from "./AddQuestion";
 import Questions from "./Questions";
 import Question from "./Question";
 import SubmitBtn from "./SubmitBtn";
+import TitleField from "./TitleField";
+import NameField from "./NameField";
+import DescriptionField from "./DescriptionField";
+import TypeField from "./TypeField";
+import CanSkipNextQuestionField from "./CanSkipNextQuestionField";
+import SkipableField from "./SkipableField";
+import ChoicesField from "./ChoicesField";
+import SkipOnField from "./SkipOnField";
 
-export { Heading, AddQuestion, Question, Questions, SubmitBtn };
+export {
+  Heading,
+  AddQuestion,
+  Question,
+  Questions,
+  SubmitBtn,
+  TitleField,
+  NameField,
+  DescriptionField,
+  TypeField,
+  CanSkipNextQuestionField,
+  SkipableField,
+  ChoicesField,
+  SkipOnField,
+};
