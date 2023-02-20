7c91896318cb28bd58fdfb00c264da2a09ddaad3
Alhassan Tijani <jhaeti@gmail.com>
2023-02-20 10:23:22 +0000

    Heading component

---
+++ src/views/page-1/components/Heading.jsx
@@ -0,0 +1,11 @@
+const Heading = () => {
+  return (
+    <>
+      <div className="intro-y flex items-center mt-8">
+        <h2 className="text-lg font-medium mr-auto">Build Questionnaire</h2>
+      </div>
+    </>
+  );
+};
+
+export default Heading;
