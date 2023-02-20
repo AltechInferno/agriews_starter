6a530905da5dfd3b4372c36009f9a8afd85f8478
Alhassan Tijani <jhaeti@gmail.com>
2023-02-20 10:24:01 +0000

    Imported all components into this and then export them from here

---
+++ src/views/page-1/components/index.js
@@ -0,0 +1,7 @@
+import Heading from "./Heading";
+import AddQuestion from "./AddQuestion";
+import Questions from "./Questions";
+import Question from "./Question";
+import SubmitBtn from "./SubmitBtn";
+
+export { Heading, AddQuestion, Question, Questions, SubmitBtn };
