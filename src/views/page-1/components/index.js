94517cb99fc9659009f5215d89d1934f88482621
Alhassan Tijani <jhaeti@gmail.com>
2023-02-23 09:52:01 +0000

    Added and exported more component

--- src/views/page-1/components/index.js
+++ src/views/page-1/components/index.js
@@ -4,13 +4,17 @@ import Questions from "./Questions";
 import Question from "./Question";
 import SubmitBtn from "./SubmitBtn";
 import TitleField from "./TitleField";
-import NameField from "./NameField";
+import TextField from "./TextField";
 import DescriptionField from "./DescriptionField";
 import TypeField from "./TypeField";
 import CanSkipNextQuestionField from "./CanSkipNextQuestionField";
 import SkipableField from "./SkipableField";
 import ChoicesField from "./ChoicesField";
 import SkipOnField from "./SkipOnField";
+import RequiredField from "./RequiredField";
+import Questionnaires from "./Questionnaires";
+import AddQuestionnaire from "./AddQuestionnaire";
+import QuestionnaireTitleField from "./QuestionnaireTitleFIeld";
 
 export {
   Heading,
@@ -19,11 +23,15 @@ export {
   Questions,
   SubmitBtn,
   TitleField,
-  NameField,
+  TextField,
   DescriptionField,
   TypeField,
   CanSkipNextQuestionField,
   SkipableField,
   ChoicesField,
   SkipOnField,
+  RequiredField,
+  Questionnaires,
+  AddQuestionnaire,
+  QuestionnaireTitleField,
 };
